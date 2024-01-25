a7013b5a7f7b79b1821c806b6ff56317c3904ef0
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2024-01-25 11:49:27 +0100

    Bank Structure

---
+++ Bank.java
@@ -0,0 +1,52 @@
+public class Bank {
+    String name;
+    String location;
+    int accountNumber;
+    int bvn;
+    double balance;
+    String state;
+    int atmPin;
+    int transferPin;
+    String country;
+    String DOB;
+    String emailAddress;
+    String address;
+    String phoneNumber;
+    String nextOfKin;
+    String gender;
+    String accountType;
+    String occupation;
+    final int ADMINPIN = 1002;
+
+    //Customer class
+    public void createAccount() {}
+    //Customer class
+    public void editAccount() {}
+    //Customer class
+    public void viewAccount() {}
+    //Customer class
+    public void checkBalance() {}
+    //Customer class
+    public void deleteAccount() {}
+    //Customer class
+    public void login() {}
+    //Customer class
+    public void logout() {}
+    //Transaction class
+    public void deposit() {}
+    //Transaction class
+    public void withdrawal() {}
+    //Transaction class
+    public void loanRequest() {}
+    //Transaction class
+    public void transfer() {}
+    //Admin class
+    public void freezeAccount() {}
+    //Admin class
+    public void releaseAccount() {}
+    //Admin class
+    public void viewCustomers() {}
+    //Admin class
+    public void creditAccount() {}
+
+}
