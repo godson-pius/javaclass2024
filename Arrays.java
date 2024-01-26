1b4e6c10788774bb268972d656c19e5cfcc71d4b
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2024-01-26 12:28:39 +0100

    MutidimensionalArray

--- Arrays.java
+++ Arrays.java
@@ -2,28 +2,55 @@ import java.util.Scanner;
 
 public class Arrays {
     public static void main(String[] args) {
-        int[] scores = new int[5];
+        String[] students = new String[5];
 
-        // scores[0] = 10;
-        // scores[1] = 50;
-        // scores[2] = 100;
-        // scores[3] = 150;
-        // scores[4] = 20;
+        // students[0] = "string";
+        // students[1] = "string";
+        // students[2] =  "string";
+        // students[3] =  "string";
+        // students[4] = "string";
 
         Scanner scan = new Scanner(System.in);
-        
-        for (int i = 0; i < scores.length; i++) {
-            System.out.printf("Enter %d Number: ", i + 1);
-            scores[i] = scan.nextInt();
+
+        for (int i = 0; i < students.length; i++) {
+            students[i] = scan.next();
         }
 
-        // for (int num : scores) {
-        //     System.out.println(num);
-        // }
-
-        System.out.println(scores[4]);
+        for (String uzo : students) {
+            System.out.println(uzo);
+        }
+        
 
         scan.close();
 
+    }
+}
+
+class MultiDimensionalArray {
+    int[][] multi = new int[3][4];
+
+    MultiDimensionalArray() {
+        // multi[0][0] = 10;
+        // multi[0][1] = 20;
+        // multi[0][2] = 5;
+        // multi[0][3] = 6;
+        // multi[1][0] = 77;
+        // multi[1][1] = 11;
+        // multi[1][2] = 10;
+        // multi[1][3] = 7;
+        // multi[2][0] = 89;
+        // multi[2][1] = 40;
+        // multi[2][2] = 41;
+        // multi[2][3] = 32;
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("Enter column number: ");
+                multi[i][j] = input.nextInt();
+            }
+        }
+
+        input.close();
     }
 }
