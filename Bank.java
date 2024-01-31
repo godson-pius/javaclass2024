c4cb5c83f8f8856a42e69c4355354fe61e7b76e0
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2024-01-31 12:36:14 +0100

    Bank file updated

--- Bank.java
+++ Bank.java
@@ -1,13 +1,11 @@
+import java.util.Scanner;
+
 public class Bank {
     String name;
     String location;
-
-    // Not fulfilled... RAND
     int accountNumber;
-    // Not fulfilled... RAND
     int bvn;
     double balance = 0.00;
-    
     String state;
     int atmPin;
     int transferPin;
@@ -22,37 +20,100 @@ public class Bank {
     String occupation;
     final int ADMINPIN = 1002;
 
-    //Create a constructor...
+    static Scanner input = new Scanner(System.in);
+
+    // Create a constructor...
     Bank() {
         System.out.println("Welcome to Laptop Bank (LB)");
     }
-    
-    //Customer class
-    public void editAccount() {}
-    //Customer class
-    public void viewAccount() {}
-    //Customer class
-    public void checkBalance() {}
-    //Customer class
-    public void deleteAccount() {}
-    //Customer class
-    public void login() {}
-    //Customer class
-    public void logout() {}
-    //Transaction class
-    public void deposit() {}
-    //Transaction class
-    public void withdrawal() {}
-    //Transaction class
-    public void loanRequest() {}
-    //Transaction class
-    public void transfer() {}
-    //Admin class
-    public void freezeAccount() {}
-    //Admin class
-    public void releaseAccount() {}
-    //Admin class
-    public void viewCustomers() {}
-    //Admin class
-    public void creditAccount() {}
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void landing() {
+        Customer customer = new Customer();
+
+        System.out.println("1. Create an account with LB");
+        System.out.println("2. Sign In to your LB account");
+        System.out.println("3. Change transfer PIN");
+        System.out.println("4. Check your LB balance");
+        System.out.println("5. View BVN");
+        System.out.println("6. Deposit funds");
+        System.out.println("7. Transfer funds");
+        System.out.println("8. Withdraw funds");
+        System.out.println("9. Request for loan");
+        System.out.println("10. Quit app");
+
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                customer.createAccount();
+                break;
+            case 2:
+                // Sign In.
+            case 10:
+                System.out.println("Thank you for banking with Laptop Bank..");
+            default:
+                System.out.println("Invalid choice....");
+                break;
+        }
+
+    }
+
+    // Customer class
+    public void editAccount() {
+    }
+
+    // Customer class
+    public void viewAccount() {
+    }
+
+    // Customer class
+    public void checkBalance() {
+    }
+
+    // Customer class
+    public void deleteAccount() {
+    }
+
+    // Customer class
+    public void login() {
+    }
+
+    // Customer class
+    public void logout() {
+    }
+
+    // Transaction class
+    public void deposit() {
+    }
+
+    // Transaction class
+    public void withdrawal() {
+    }
+
+    // Transaction class
+    public void loanRequest() {
+    }
+
+    // Transaction class
+    public void transfer() {
+    }
+
+    // Admin class
+    public void freezeAccount() {
+    }
+
+    // Admin class
+    public void releaseAccount() {
+    }
+
+    // Admin class
+    public void viewCustomers() {
+    }
+
+    // Admin class
+    public void creditAccount() {
+    }
 }
